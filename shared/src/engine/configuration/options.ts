351374c25bc0c09eadfc4f37bb6517f8ea5639b5
LBO44 <146635915+LBO44@users.noreply.github.com>
2025-02-28 22:31:15 +0100

    support Sonolus 0.8.11

--- shared/src/engine/configuration/options.ts
+++ shared/src/engine/configuration/options.ts
@@ -3,11 +3,13 @@ import { EngineConfigurationOption } from '@sonolus/core'
 export const optionsDefinition = {
   bgm: {
     name: "Background Music",
+    description: "Adds a nice background music to make the gameplay even more enjoyable! ♪♪",
     type: "toggle",
     def: 1
   },
   dpad: {
     name: "Movement Method",
+    description: "Whether you want to move around by swiping on the screen or by pressing the Dpad's buttons.",
     type: "select",
     def: 0,
     values: ["Swipe", "Dpad"]
@@ -22,6 +24,7 @@ export const optionsDefinition = {
   },
   noWall: {
     name: "Disable Walls",
+    description: "Tired of constantly hitting the grid's walls?\nTry this option, it will wrap you to the other side instead!",
     type: "toggle",
     def: 0,
     standard: true,
